607001adccf1cdc09cc40e280cccfe944e010139
Pablo Juárez <87239199+psebas99@users.noreply.github.com>
2023-12-06 13:53:15 -0600

    Add files via upload

---
+++ TAREA2.m
@@ -0,0 +1,35 @@
+%Generar señal senoidal
+fs= 1000; %frecuencia de muestreo
+t = 0:1/fs:1; %vector de tiempo
+f = 100; %frecuencia de la señal
+x = sin(2*pi*f*t); %señal senoidal
+
+%Aplicar Transformada de fourier
+xf = fft(x);
+
+%Generar filtro pasa-bajo
+n= length(x);
+fcutoff = 50; %frecuencia de corte
+h = ones(n,1); %vector de ceros
+h(round(n*fcutoff/fs)+1:end) = 0; %aplicar filtro pasa-bajo
+
+%Aplicar filtro a la señal en el dominio de la frecuencia
+xf_filtered = xf .*h;
+
+%Convertir señal filtrada a dominio del tiempo
+x_filtered = ifft(xf_filtered);
+
+%Graficar sela original y señal filtrada
+figure;
+subplot(2,1,1);
+plot(t, x);
+title('Señal original');
+xlabel('Tiempo (s)');
+ylabel('Amplitud');
+subplot(2,1,2);
+plot(t, real(x_filtered));
+title('Señal Filtrada');
+xlabel('Tiempo (s)');
+ylabel('Amplitud');
+
+
